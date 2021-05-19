69d5447576a8a85244f86385f0363663ec8db667
Salih <salih285@gmail.com>
2021-05-20 00:36:14 +0200

    testing

--- .github/workflows/ipynb_to_html.sh
+++ .github/workflows/ipynb_to_html.sh
@@ -1,7 +1,4 @@
 
 MAIN_DIR=$(pwd)
-echo "I am in $MAIN_DIR now!"
-echo "This folder includes:"
-ls
-git status
-git status --porcelain | sed 's/[^ ]* *//' | grep .ipynb
+
+git diff --name-only HEAD HEAD~1 |  grep .ipynb
